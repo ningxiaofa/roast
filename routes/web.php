dd9d35db01b61f2cc83f0704095374acaf54fd0a
baby <baby@example.com>
2020-01-08 17:19:27 +0800

    实现基于 Github 进行登录认证[升级laravel至5.8, 通过ssl 下载ca证书]

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,15 @@
 |
 */
 
-Route::get('/', 'Web\AppController@getApp')->middleware('auth');
+Route::get('/', 'Web\AppController@getApp')
+    ->middleware('auth'); //本应用只允许认证用户才能访问，所以我们在入口路由上使用了 auth 中间件。
+
+Route::get('/login', 'Web\AppController@getLogin' )
+    ->name('login')
+    ->middleware('guest'); //guest中间件的用途是登录用户访问该路由会跳转到指定认证后页面，而非登录用户访问才会显示登录页面。
+
+Route::get( '/auth/{social}', 'Web\AuthenticationController@getSocialRedirect' )
+    ->middleware('guest');
+
+Route::get( '/auth/{social}/callback', 'Web\AuthenticationController@getSocialCallback' )
+    ->middleware('guest');
