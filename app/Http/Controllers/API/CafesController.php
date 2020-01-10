44e4d23728b2f1836752081f0b6e45e2c1bc5cb6
baby <baby@example.com>
2020-01-10 18:21:52 +0800

    通过 JavaScript 和 Laravel 验证表单请求

--- app/Http/Controllers/API/CafesController.php
+++ app/Http/Controllers/API/CafesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Request;
 use App\Models\Cafe;
+use App\Http\Requests\StoreCafeRequest;
 
 class CafesController extends Controller
 {
@@ -44,16 +45,16 @@ class CafesController extends Controller
      | Method:         POST
      | Description:    Adds a new cafe to the application
     */
-    public function postNewCafe(){
+    public function postNewCafe(StoreCafeRequest $request){
         //为了简化业务逻辑，这里假定用户提交的数据都是可靠的，将数据验证逻辑省略掉
         $cafe = new Cafe();
 
         //cafes 表的其他字段都是动态添加的，无需用户手动提交。关于经度和纬度字段的获取，后续在地图 API 中会提到.
-        $cafe->name     = Request::get('name');
-        $cafe->address  = Request::get('address');
-        $cafe->city     = Request::get('city');
-        $cafe->state    = Request::get('state');
-        $cafe->zip      = Request::get('zip');
+        $cafe->name     = $request->input('name');  // Request::get('name'); 亦可
+        $cafe->address  = $request->input('address');
+        $cafe->city     = $request->input('city');
+        $cafe->state    = $request->input('state');
+        $cafe->zip      = $request->input('zip');
 
         $cafe->save();
 
